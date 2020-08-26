00fec126dacf25e1117e7a258c2320b411c3862c
Andrew Moses <andrewmosesdrive@gmail.com>
2020-08-25 22:09:41 -0700

    defined arrays, added prompts/confirms and consoles

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Constants
 const generateBtn = document.querySelector("#generate");
 
+
 // Functions
 
 /**
@@ -17,20 +18,37 @@ function writePassword() {
  * generate a password based on certain criteria
  */
 function generatePassword(){
+  // create list of possible characters
+  // input criteria from user
+  // if any condition is true, include the appropriate array
+
+  let upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  let lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  let number = ["0","1","2","3","4","5","6","7","8","9"];
+  let specialChar = ["!","@","#","$","%","^","&","*","?"];
+
+  
 
   // Password is currently blank! We need to make a better one
   let password = "";
 
-    let criteria = document.getElementById(criteriaPrompt).value;
-    let length = document.getElementById(lengthPrompt).value;
-    let character = document.getElementById(characterPrompt).value;
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890`~!@#$%^&*()_+-=";
+  let passwordLength = prompt("How long do you want your password to be?");
 
-    for(let i = 0; i <= length; i++) {
-      password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+  let confirmUppercase = confirm("Do you want to include upper case letters?");
+  let confirmLowercase = confirm("Do you want to include upper case letters?");
+  let confirmNumber = confirm("Do you want to include upper case letters?");
+  let confirmSpecialChar = confirm("Do you want to include upper case letters?");
 
-    }
+  console.log(passwordLength, confirmUppercase, confirmLowercase, confirmNumber, confirmSpecialChar)
 
+
+
+
+  // create a for loop that repeats number of userLength times
+  // math.random out of possible char options
+  // concatenate random characters
+
+    
 
 
   return password;
